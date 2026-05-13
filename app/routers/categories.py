027cf7bf88021b9b73fc32fe59f3b111517430ff
drenxhyliqi <xhyliqiidren@gmail.com>
2026-05-13 21:22:57 +0200

    CRUD operations for book categories

---
+++ app/routers/categories.py
@@ -0,0 +1,32 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from typing import Annotated
+from starlette import status
+
+# Database imports
+from app.database import get_db
+from app.models import Category
+from app.schemas import CategoryCreate, CategoryResponse
+
+# Per mos me shkru /members/... ne qdo endpoint prefix /
+router = APIRouter(prefix="/categories", tags=["categories"])
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+# GET    / categories
+# POST / categories
+
+@router.get("/", response_model=list[CategoryResponse])
+async def get_categories(db: db_dependency):
+    query = db.query(Category)
+
+    return query
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+async def create_category(db: db_dependency, category: CategoryCreate):
+    category = Category(**category.model_dump())
+    db.add(category)
+    db.commit()
+    db.refresh(category)
+    return category
