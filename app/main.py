f9d55a4f8e39b2d182b146b536d36c31ed1ff384
drenxhyliqi <xhyliqiidren@gmail.com>
2026-05-13 21:22:14 +0200

    linked categories router to main

--- app/main.py
+++ app/main.py
@@ -3,12 +3,14 @@ from typing_extensions import Annotated
 
 from fastapi import Depends, FastAPI, status
 from app.database import get_db
-from app.routers import members, books, authors
+from app.routers import members, books, authors, categories
+
 
 app = FastAPI()
 app.include_router(members.router)
 app.include_router(books.router)
 app.include_router(authors.router)
+app.include_router(categories.router)
 
 
 @app.get("/", status_code=status.HTTP_200_OK)
